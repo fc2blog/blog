86560d63a3a8624272d34bd608879611bdc1b132
Shinichiro Tanaka <kk@kimini.jp>
2020-07-07 15:39:54 +0900

    【7.4非推奨対応】マジッククオート機能の利用の削除(PHP5.4.0以降はget_magic_quotes_gpcは常にFLASEを返す)

--- app/core/common_functions.php
+++ app/core/common_functions.php
@@ -1,17 +1,5 @@
 <?php
 
-// magic_quotes対応
-if (get_magic_quotes_gpc()) {
-  function stripslashes_gpc(&$value)
-  {
-    $value = stripslashes($value);
-  }
-  array_walk_recursive($_GET, 'stripslashes_gpc');
-  array_walk_recursive($_POST, 'stripslashes_gpc');
-  array_walk_recursive($_COOKIE, 'stripslashes_gpc');
-  array_walk_recursive($_REQUEST, 'stripslashes_gpc');
-}
-
 // lcfirst関数補完
 if(function_exists('lcfirst') === false) {
   function lcfirst($str) {
