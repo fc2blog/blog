e0c1f47c45332aa733a2752110c1d73d1659345c
ShinichiroTanaka <tanaka.kimini@gmail.com>
2020-07-21 10:36:48 +0900

    #13～#15：選択チェックボックスを選択し「選択したものを全て削除する」ボタンから削除しようとしても削除されない不具合修正（タグ一覧、記事一覧、コメント一覧）

--- app/config/routing.php
+++ app/config/routing.php
@@ -9,10 +9,15 @@ $paths = $request->getPaths();
 $argsc = Config::get('ARGS_CONTROLLER');
 $argsa = Config::get('ARGS_ACTION');
 
-if (isset($paths[1])) {
+if ($request->isArgs($argsc)) {
+  $request->set($argsc, $request->get($argsc));
+} elseif (isset($paths[1])) {
   $request->set($argsc, $paths[1]);
 }
-if (isset($paths[2])) {
+
+if ($request->isArgs($argsa)) {
+  $request->set($argsa, $request->get($argsa));
+} elseif (isset($paths[2])) {
   $request->set($argsa, $paths[2]);
 }
 
