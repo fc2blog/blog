791196ff57b4e34f864b4728f6a26a099fdf39db
uzulla <uzulla@himitsukichi.com>
2020-07-24 13:39:20 +0900

    Add: test helpers.

---
+++ tests/LoaderHelper.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Fc2blog\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class LoaderHelper extends TestCase
+{
+  public static function requireBootStrap()
+  {
+    if (file_exists(TEST_APP_DIR . "/../public/config.php")) {
+      require_once(TEST_APP_DIR . "/../public/config.php");
+    } else {
+      require_once(TEST_APP_DIR . "/../docker/docker.config.php");
+    }
+    require_once(TEST_APP_DIR . "/core/bootstrap.php");
+  }
+}
