c202c80d97055de2815597f164cd6c2c5470881f
uzulla <uzulla@himitsukichi.com>
2020-07-25 22:05:15 +0900

    Add test: GetSchemaByBlogIdTest

--- tests/App/Model/BlogsModel/GetSchemaByBlogIdTest.php
+++ tests/App/Model/BlogsModel/GetSchemaByBlogIdTest.php
@@ -5,9 +5,7 @@ namespace Fc2blog\Tests\App\Model\BlogsModel;
 
 use BlogsModel;
 use Config;
-use Fc2blog\Tests\DBHelper;
 use Fc2blog\Tests\LoaderHelper;
-use InvalidArgumentException;
 use Model;
 use PHPUnit\Framework\TestCase;
 
@@ -23,25 +21,12 @@ class GetSchemaByBlogIdTest extends TestCase
       Model::load('blogs');
     }
 
-    DBHelper::clearDbAndInsertFixture();
-
     parent::setUp();
   }
 
-  public function testCorrectSchemaStrings(): void
+  public function testGetSchemaByBlogIdTest(): void
   {
-    $this->assertEquals('https:', BlogsModel::getSchemaByBlogId("testblog1"));
-    $this->assertEquals('http:', BlogsModel::getSchemaByBlogId("testblog2"));
+    $this->assertEquals('https:', BlogsModel::getSchemaByBlogId('testblog1'));
+    $this->assertEquals('http:', BlogsModel::getSchemaByBlogId('testblog2'));
   }
-
-  public function testMissingBlogId(): void
-  {
-    try {
-      BlogsModel::getSchemaByBlogId("--missing-blog-id--");
-      $this->fail("存在しない場合は例外が投げられる");
-    } catch (InvalidArgumentException $e) {
-      $this->assertTrue(true);
-    }
-  }
-
 }
