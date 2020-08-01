f4d4df42f6ae58774e216909a7ba1d86bdbc8cee
uzulla <uzulla@himitsukichi.com>
2020-08-01 22:57:24 +0900

    Improve cookie security: enable "httponly" on session cookie. remove "SESSION_DEFAULT_DOMAIN" from default config. add samesite support.

---
+++ tests/App/Core/Cookie/SetTest.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace Fc2blog\Tests\App\Core\Cookie;
+
+use Cookie;
+use Fc2blog\Tests\LoaderHelper;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+class SetTest extends TestCase
+{
+  public function setUp(): void
+  {
+    LoaderHelper::requireBootStrap();
+    parent::setUp();
+  }
+
+  public function testSetString(): void
+  {
+    // Cookieの正しいテストはUnitTestでは困難です
+    // echo '<?php setcookie("1","b");' | php-cgi
+    // 等でテストができますが、php-cgiがない環境でテストが実行できません。
+    @Cookie::set("k", "v");
+    $this->assertTrue(true);
+  }
+
+  public function testSetInvalidType(): void
+  {
+    try {
+      /** @noinspection PhpStrictTypeCheckingInspection */
+      Cookie::set(true, "v");
+      $this->fail();
+    } catch (TypeError $e) {
+      $this->assertInstanceOf(TypeError::class, $e);
+    }
+
+    try {
+      /** @noinspection PhpStrictTypeCheckingInspection */
+      Cookie::set(1, "v");
+      $this->fail();
+    } catch (TypeError $e) {
+      $this->assertInstanceOf(TypeError::class, $e);
+    }
+
+    try {
+      /** @noinspection PhpStrictTypeCheckingInspection */
+      Cookie::set("k", true);
+      $this->fail();
+    } catch (TypeError $e) {
+      $this->assertInstanceOf(TypeError::class, $e);
+    }
+
+    try {
+      /** @noinspection PhpStrictTypeCheckingInspection */
+      Cookie::set("k", 1);
+      $this->fail();
+    } catch (TypeError $e) {
+      $this->assertInstanceOf(TypeError::class, $e);
+    }
+  }
+
+  public function testDenyInvalidSamesite(): void
+  {
+    try {
+      @Cookie::set("k", "v", time(), "", "", false, false, "Lax");
+      $this->assertTrue(true);
+    } catch (InvalidArgumentException $e) {
+      $this->fail($e->getMessage());
+    }
+    try {
+      @Cookie::set("k", "v", time(), "", "", false, false, "Strict");
+      $this->assertTrue(true);
+    } catch (InvalidArgumentException $e) {
+      $this->fail($e->getMessage());
+    }
+    try {
+      $_SERVER['HTTPS'] = "on";
+      @Cookie::set("k", "v", time(), "", "", true, false, "None");
+      $this->assertTrue(true);
+    } catch (InvalidArgumentException $e) {
+      $this->fail($e->getMessage());
+    } finally {
+      unset($_SERVER['HTTPS']);
+    }
+    try {
+      @Cookie::set("k", "v", time(), "", "", false, false, "Wrong");
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+      $this->assertTrue(true);
+    }
+  }
+
+  public function testSamesiteNoneAndNotSecure(): void
+  {
+    try {
+      @Cookie::set("k", "v", time(), "", "", false, false, "None");
+      $this->fail();
+    } catch (InvalidArgumentException $e) {
+      $this->assertTrue(true);
+    }
+  }
+}
