38a7619782b9eb36c24887ddd27295ed518046ac
uzulla <uzulla@himitsukichi.com>
2020-08-15 15:40:46 +0900

    Psr4ize: remove unnecessary require().

--- app/include/admin_install_include.php
+++ app/include/admin_install_include.php
@@ -8,5 +8,4 @@ require(__DIR__ . '/config_loading_include.php');
 
 \Fc2blog\Config::read('admin.php'); // Admin用の環境設定読み込み
 
-require(\Fc2blog\Config::get('CONTROLLER_DIR') . 'admin/common_controller.php');
-$controller = new CommonController('install');
+$controller = new \Fc2blog\Web\Controller\Admin\CommonController('install');
