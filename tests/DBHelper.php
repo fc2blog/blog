97ad655c99ccc2122688954dbe308f27e7bd58b4
uzulla <uzulla@himitsukichi.com>
2020-08-17 15:29:04 +0900

    move config/blog.sql to db/0_initialize.sql

--- tests/DBHelper.php
+++ tests/DBHelper.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace Fc2blog\Tests;
 
-
 use Fc2blog\Config;
 use Fc2blog\Model\MSDB;
 use PDO;
@@ -13,23 +12,27 @@ class DBHelper extends TestCase
 {
   public static function clearDbAndInsertFixture()
   {
-    $msdb = MSDB::getInstance();
+    static::clearDb();
+
+    $pdo = new PDO("mysql:host=" . Config::get('MASTER_DB.HOST') . ";port=3306;dbname=" . Config::get('MASTER_DB.DATABASE') . ";charset=utf8mb4", Config::get('MASTER_DB.USER'), Config::get('MASTER_DB.PASSWORD'));
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // DB接続確認(DATABASEの存在判定含む)
-    $msdb->connect();
-
-    // load DDL (DBが初期化されます！)
-    $sql_path = Config::get('CONFIG_DIR') . 'blog.sql';
-    $sql = file_get_contents($sql_path);
+    $sql = file_get_contents(Config::get('APP_DIR') . 'db/0_initialize.sql');
     if (DB_CHARSET != 'UTF8MB4') {
       $sql = str_replace('utf8mb4', strtolower(DB_CHARSET), $sql);
     }
-    MSDB::getInstance()->multiExecute($sql);
+    $pdo->query($sql);
 
     // load fixture
     $sql = file_get_contents(__DIR__ . "/test_fixture.sql");
+    $pdo->query($sql);
+  }
+
+  public static function clearDb()
+  {
     $pdo = new PDO("mysql:host=" . Config::get('MASTER_DB.HOST') . ";port=3306;dbname=" . Config::get('MASTER_DB.DATABASE') . ";charset=utf8mb4", Config::get('MASTER_DB.USER'), Config::get('MASTER_DB.PASSWORD'));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->query($sql);
+    $pdo->query(file_get_contents(__DIR__."/test_drop_all_table.sql"));
   }
 }
