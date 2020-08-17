f239c57f35b0d72287032abb1956a02b798bbcff
uzulla <uzulla@himitsukichi.com>
2020-08-17 12:53:50 +0900

    Refactor: unite app.php dir.php env.php

--- app/config/init_config.php
+++ app/config/init_config.php
@@ -1,9 +1,125 @@
 <?php
-/**
-* アプリの定数系設定ファイル
-*/
 
-$config = array();
+$config = [];
+
+// -------------------- ディレクトリ構造関連 --------------------//
+
+$config['ROOT_DIR'] = realpath(dirname(__FILE__) . '/../../') . '/';
+
+$config['UPLOAD_DIR_NAME'] = 'uploads';
+
+$config['WWW_DIR']        = defined('WWW_DIR') ? WWW_DIR : $config['ROOT_DIR'] . 'public/';
+$config['WWW_UPLOAD_DIR'] = $config['WWW_DIR'] . $config['UPLOAD_DIR_NAME'] . '/';
+
+$config['APP_DIR']           = defined('APP_DIR') ? APP_DIR : $config['ROOT_DIR'] . 'app/';
+$config['CORE_DIR']          = $config['APP_DIR']  . 'core/';
+$config['CONFIG_DIR']        = $config['APP_DIR']  . 'config/';
+$config['LOCALE_DIR']        = $config['APP_DIR']  . 'locale/';
+$config['CONTROLLER_DIR']    = $config['APP_DIR']  . 'controller/';
+$config['MODEL_DIR']         = $config['APP_DIR']  . 'model/';
+$config['VIEW_DIR']          = $config['APP_DIR']  . 'view/';
+$config['VIEW_COMMON_DIR']   = $config['VIEW_DIR'] . 'common/';
+$config['LIB_DIR']           = $config['APP_DIR']  . 'lib/';
+$config['TEMP_DIR']          = $config['APP_DIR']  . 'temp/';
+$config['BLOG_TEMPLATE_DIR'] = $config['TEMP_DIR'] . 'blog_template/';
+
+// -------------------- DB関連 --------------------//
+// Master/Slave機能のON/OFF
+$config['IS_MASTER_SLAVE'] = false;
+$config['DB_CHARSET'] = DB_CHARSET;
+
+// DBの接続情報
+$config['MASTER_DB'] = array(
+  'HOST' => DB_HOST,
+  'USER' => DB_USER,
+  'PASSWORD' => DB_PASSWORD,
+  'DATABASE' => DB_DATABASE,
+);
+
+$config['SLAVE_DB'] = array(
+  'HOST' => 'localhost',
+  'USER' => 'root',
+  'PASSWORD' => '',
+  'DATABASE' => 'blog_slave',
+);
+
+// -------------------- Debug関連 --------------------//
+// Debug 0 = false 1 = echo 2 = html 3 = link
+$config['DEBUG'] = (defined("THIS_IS_TEST")) ? 5 : 0;                  // Debugの表示可否
+$config['DEBUG_TEMPLATE_VARS'] = 0;    // テンプレートで使用可能な変数の一覧表示
+
+// -------------------- 色々 --------------------//
+// 言語設定
+$config['LANG'] = 'ja';
+
+// 国際化対応用
+$config['LANGUAGE'] = 'ja_JP.UTF-8';
+
+// 国際化対応用の対応言語一覧
+$config['LANGUAGES'] = array(
+  'ja' => 'ja_JP.UTF-8',
+  'en' => 'en_US.UTF-8',
+);
+
+// エディタの言語切り替え互換用
+$config['LANG_ELRTE'] = array(
+  'ja' => 'jp',
+  'en' => 'en',
+);
+
+// タイムゾーン
+$config['TIMEZONE'] = 'Asia/Tokyo';
+
+// 内部エンコード
+$config['INTERNAL_ENCODING'] = 'UTF-8';
+
+// cron実行
+$config['CRON'] = false;
+
+// パスワード用のsalt
+$config['PASSWORD_SALT'] = PASSWORD_SALT;
+
+// ドメイン
+$config['DOMAIN'] = DOMAIN;
+$config['DOMAIN_USER'] = $config['DOMAIN'];
+$config['DOMAIN_ADMIN'] = $config['DOMAIN'];
+
+// ポート
+$config['HTTP_PORT_STR'] = (HTTP_PORT === "80") ? '' : ":" . HTTP_PORT; // http時、80は省略できる
+$config['HTTPS_PORT_STR'] = (HTTP_PORT === "443") ? '' : ":" . HTTPS_PORT; // https時、443は省略できる
+
+// Sessionのデフォルト有効ドメイン
+$config['SESSION_DEFAULT_DOMAIN'] = ""; // 省略時、アクセスドメインとなります
+
+// SESSIONのID名
+$config['SESSION_NAME'] = 'dojima';
+
+// Cookieのデフォルト有効ドメイン
+$config['COOKIE_DEFAULT_DOMAIN'] = ""; // JS用Cookie名 省略時、アクセスドメインとなります
+$config['COOKIE_EXPIRE'] = 180; // 有効期限 180日
+
+// URLの形式を書き換えるフラグ
+$config['URL_REWRITE'] = false;
+
+// ベースディレクトリ
+$config['BASE_DIRECTORY'] = '/';
+
+// directory indexファイル名
+$config['DIRECTORY_INDEX'] = 'index.php';
+
+// Controller引数
+$config['ARGS_CONTROLLER'] = 'mode';
+
+// Action引数
+$config['ARGS_ACTION'] = 'process';
+
+// ルーティング設定
+$config['ROUTING'] = 'routing.php';
+
+// アプリプレフィックス(Controller,Viewのプレフィックス)
+$config['APP_PREFIX'] = null;
+
+// -------------------- アプリの定数系設定ファイル --------------------//
 
 // デバイスタイプ
 $config['DEVICE_PC'] = 1;   // PC
