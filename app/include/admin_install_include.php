b765c7e456dbc0c7fc2f3c898ef8556471632955
uzulla <uzulla@himitsukichi.com>
2020-08-19 02:21:33 +0900

    WIP:Big restructuring: apply installer

--- app/include/admin_install_include.php
+++ app/include/admin_install_include.php
@@ -6,6 +6,8 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 
 require(__DIR__ . '/config_loading_include.php');
 
+$request = new \Fc2blog\Web\Request();
+
 \Fc2blog\Config::read('admin.php'); // Admin用の環境設定読み込み
 
-$controller = new \Fc2blog\Web\Controller\Admin\CommonController('install');
+$controller = new \Fc2blog\Web\Controller\Admin\CommonController($request,'install');
