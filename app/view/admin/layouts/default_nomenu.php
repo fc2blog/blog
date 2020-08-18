3311f292dcfe71979b9d04f2fad31201a85c814d
uzulla <uzulla@himitsukichi.com>
2020-08-19 02:23:22 +0900

    WIP:Big restructuring: apply installer

--- app/view/admin/layouts/default_nomenu.php
+++ app/view/admin/layouts/default_nomenu.php
@@ -28,7 +28,7 @@
     <?php endif; ?>
     common.baseDirectory = '<?php echo \Fc2blog\Config::get('BASE_DIRECTORY'); ?>';
     common.deviceType = <?php echo $this->getDeviceType(); ?>;
-    common.deviceArgs = '<?php echo \Fc2blog\App::getArgsDevice(); ?>';
+    common.deviceArgs = '<?php echo \Fc2blog\App::getArgsDevice($request); ?>';
   </script>
 
   <?php echo $this->includeJS(); ?>
