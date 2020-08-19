3bfbaff371ef279e5fd90c725ea805a7345af1b6
uzulla <uzulla@himitsukichi.com>
2020-08-19 19:29:14 +0900

    WIP:Big restructuring: remove dependency config/admin.php from admin/install.php

--- app/config/admin.php
+++ app/config/admin.php
@@ -1,17 +1,17 @@
 <?php
-// TODO move configu to /admin/install.php. and delete this file. 
-declare(strict_types=1);
-
-$config = [];
-
-$config['APP_PREFIX'] = 'Admin';
-
-$config['DEFAULT_CLASS_NAME'] = 'CommonController';
-// DELME $config['DEFAULT_METHOD_NAME'] = 'index';
-$config['CLASS_PREFIX'] = "\\Fc2blog\\Web\\Controller\\Admin\\";
-
-$config['URL_REWRITE'] = true;
-
-$config['BASE_DIRECTORY'] = '/admin/';
-
-return $config;
+// DELME
+//declare(strict_types=1);
+//
+//$config = [];
+//
+//$config['APP_PREFIX'] = 'Admin';
+//
+//$config['DEFAULT_CLASS_NAME'] = 'CommonController';
+//// DELME $config['DEFAULT_METHOD_NAME'] = 'index';
+//$config['CLASS_PREFIX'] = "\\Fc2blog\\Web\\Controller\\Admin\\";
+//
+//$config['URL_REWRITE'] = true;
+//
+//$config['BASE_DIRECTORY'] = '/admin/';
+//
+//return $config;
--- app/include/admin_install_include.php
+++ app/include/admin_install_include.php
@@ -8,6 +8,8 @@ require(__DIR__ . '/config_loading_include.php');
 
 $request = new \Fc2blog\Web\Request();
 
-\Fc2blog\Config::read('admin.php'); // Admin用の環境設定読み込み
+\Fc2blog\Config::set('URL_REWRITE', true);
+\Fc2blog\Config::set('BASE_DIRECTORY', '/admin/');
+\Fc2blog\Config::set('APP_PREFIX', 'Admin');
 
 $controller = new \Fc2blog\Web\Controller\Admin\CommonController($request,'install');
