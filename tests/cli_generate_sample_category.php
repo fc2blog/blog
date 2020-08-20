ed51b806237c252d2752787b06c240a54cc7434e
uzulla <uzulla@himitsukichi.com>
2020-08-20 23:18:18 +0900

    gen test data: Refactoring

--- tests/cli_generate_sample_category.php
+++ tests/cli_generate_sample_category.php
@@ -1,9 +1,18 @@
 #!/usr/bin/env php
 <?php
+declare(strict_types=1);
 
+use Fc2blog\Config;
 use Fc2blog\Tests\Helper\SampleDataGenerator\GenerateSampleCategory;
+use Fc2blog\Tests\LoaderHelper;
 
-include __DIR__ . "/bootstrap.php";
+define("TEST_APP_DIR", __DIR__ . "/../app");
+
+require_once __DIR__ . "/../app/vendor/autoload.php";
+
+LoaderHelper::requireBootStrap();
+
+Config::set('DEBUG', 0); // suppress debug message.
 
 $generator = new GenerateSampleCategory();
 
