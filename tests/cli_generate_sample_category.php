8b57840ad7df0cbee6ffbb3f98728dfeb454acfc
uzulla <uzulla@himitsukichi.com>
2020-08-21 08:36:54 +0900

    gen test data: remove sample

--- tests/cli_generate_sample_category.php
+++ tests/cli_generate_sample_category.php
@@ -19,13 +19,13 @@ $generator = new GenerateSampleCategory();
 # データ生成例
 $generator->generateSampleCategories('testblog2');
 
-# リスト取得例
-$list = $generator->getCategoryList('testblog2');
-var_dump($list);
-
-# 全削除例
-$generator->syncRemoveAllCategories('testblog2');
-
-# 全削除の確認
-$list = $generator->getCategoryList('testblog2');
-var_dump($list);
+//# リスト取得例
+//$list = $generator->getCategoryList('testblog2');
+//var_dump($list);
+//
+//# 全削除例
+//$generator->syncRemoveAllCategories('testblog2');
+//
+//# 全削除の確認
+//$list = $generator->getCategoryList('testblog2');
+//var_dump($list);
