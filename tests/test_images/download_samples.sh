426fad6299601b2f14484acbf1969a226f6bd97f
uzulla <uzulla@himitsukichi.com>
2020-08-21 18:04:30 +0900

    gen test data: Add sample image download script.

---
+++ tests/test_images/download_samples.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -O 0.png https://picsum.photos/200/300
+wget -O 1.png https://picsum.photos/200/300
+wget -O 2.png https://picsum.photos/200/300
+wget -O 3.png https://picsum.photos/200/300
+wget -O 4.png https://picsum.photos/200/300
+wget -O 5.png https://picsum.photos/200/300
+wget -O 6.png https://picsum.photos/200/300
+wget -O 7.png https://picsum.photos/200/300
+wget -O 8.png https://picsum.photos/200/300
+wget -O 9.png https://picsum.photos/200/300
