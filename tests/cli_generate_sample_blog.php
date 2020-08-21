25dd70945d3147fd3af026288510cffc81aa5e9e
uzulla <uzulla@himitsukichi.com>
2020-08-21 13:17:52 +0900

    gen test data: Add GenerateSampleBlog

---
+++ tests/cli_generate_sample_blog.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+
+use Fc2blog\Config;
+use Fc2blog\Tests\Helper\SampleDataGenerator\GenerateSampleBlog;
+use Fc2blog\Tests\LoaderHelper;
+
+define("TEST_APP_DIR", __DIR__ . "/../App");
+
+require_once __DIR__ . "/../app/vendor/autoload.php";
+
+LoaderHelper::requireBootStrap();
+
+Config::set('DEBUG', 0); // suppress debug message.
+
+$generator = new GenerateSampleBlog();
+
+# データ生成例
+$generator->generateSampleBlog(1); // 1 is admin
+
