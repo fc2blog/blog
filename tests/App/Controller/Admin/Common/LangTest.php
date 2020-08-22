96c19f5a562f62e8074add36050cd2bb2d5a4765
uzulla <uzulla@himitsukichi.com>
2020-08-22 16:28:34 +0900

    Add test: Test language change function that en to ja, ja to en at admin login page.

---
+++ tests/App/Controller/Admin/Common/LangTest.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace Fc2blog\Tests\App\Controller\Admin\Common;
+
+use Fc2blog\Tests\Helper\ClientTrait;
+use Fc2blog\Web\Request;
+use Fc2blog\Web\Session;
+use PHPUnit\Framework\TestCase;
+
+class LangTest extends TestCase
+{
+  use ClientTrait;
+
+  public function testChangeLanguageToEn(): void
+  {
+    Session::destroy(new Request());
+    $this->resetSession();
+    $this->resetCookie();
+
+    // Ja表記を確認
+    $c = $this->reqGet("/admin/users/login");
+    $this->assertEquals("管理画面へログイン", $c->get('html_title'));
+
+    // enに切り替えをリクエスト
+    $this->reqGetBeRedirect("/admin/common/lang", ["lang" => "en"]);
+    $this->assertEquals('en', $this->clientTraitCookie['lang']);
+
+    // enか確認
+    $c = $this->reqGet("/admin/users/login");
+    $this->assertEquals("Login to Administration page", $c->get('html_title'));
+  }
+
+  public function testChangeLanguageToJp(): void
+  {
+    Session::destroy(new Request());
+    $this->resetSession();
+    $this->resetCookie();
+
+    // enに設定
+    $this->clientTraitCookie['lang'] = "en";
+    $c = $this->reqGet("/admin/users/login");
+    $this->assertEquals("Login to Administration page", $c->get('html_title'));
+
+    // jaに設定
+    $this->reqGetBeRedirect("/admin/common/lang", ["lang" => "ja"]);
+    $this->assertEquals('ja', $this->clientTraitCookie['lang']);
+
+    // jaか確認
+    $c = $this->reqGet("/admin/users/login");
+    $this->assertEquals("管理画面へログイン", $c->get('html_title'));
+  }
+
+}
