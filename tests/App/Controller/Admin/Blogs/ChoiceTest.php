11324b776da1ccf696474798de85565fdb3a8e70
uzulla <uzulla@himitsukichi.com>
2020-08-22 17:04:40 +0900

    add test: The function to change the blog currently selected on the admin page.

---
+++ tests/App/Controller/Admin/Blogs/ChoiceTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace Fc2blog\Tests\App\Controller\Admin\Blogs;
+
+use Fc2blog\Tests\Helper\ClientTrait;
+use Fc2blog\Web\Controller\Admin\BlogsController;
+use Fc2blog\Web\Request;
+use Fc2blog\Web\Session;
+use PHPUnit\Framework\TestCase;
+
+class ChoiceTest extends TestCase
+{
+  use ClientTrait;
+
+  public function testDefaultSelectedBlog(): void
+  {
+    Session::destroy(new Request());
+    $this->resetSession();
+    $this->resetCookie();
+    $this->mergeAdminSession();
+
+    // Ja表記を確認
+    $c = $this->reqGet("/admin/common/notice");
+    $this->assertStringContainsString("<title>testblog2</title>", $c->getOutput());
+    $this->assertEquals("testblog2", $this->clientTraitSession['blog_id']);
+  }
+
+  public function testChangeSelectedBlog(): void
+  {
+    $this->mergeAdminSession();
+    $this->assertEquals("testblog2", $this->clientTraitSession['blog_id']);
+
+    $r = $this->reqGetBeRedirect("/admin/blogs/choice", ['blog_id' => 'testblog1']);
+    $this->assertEquals('/admin/', $r->redirectUrl);
+    $this->assertEquals("testblog1", $this->clientTraitSession['blog_id']);
+  }
+
+  public function testCreateNewBlog(): void
+  {
+    $this->mergeAdminSession();
+    $this->assertEquals("testblog2", $this->clientTraitSession['blog_id']);
+
+    $c = $this->reqGet("/admin/blogs/create");
+    $this->assertInstanceOf(BlogsController::class, $c);
+    $this->assertEquals('create', $c->getResolvedMethod());
+    $this->assertEquals('admin/blogs/create.php', $c->getTemplateFilePath());
+  }
+}
