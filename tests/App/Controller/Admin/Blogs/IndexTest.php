0c1cd795cbacc2928e8304727687ae05d017af1d
uzulla <uzulla@himitsukichi.com>
2020-08-24 15:52:14 +0900

    add test: /admin/blogs/index

--- tests/App/Controller/Admin/Blogs/IndexTest.php
+++ tests/App/Controller/Admin/Blogs/IndexTest.php
@@ -3,8 +3,9 @@ declare(strict_types=1);
 
 namespace Fc2blog\Tests\App\Controller\Admin\Blogs;
 
+use Fc2blog\Tests\DBHelper;
 use Fc2blog\Tests\Helper\ClientTrait;
-use Fc2blog\Web\Controller\Admin\CommonController;
+use Fc2blog\Web\Controller\Admin\BlogsController;
 use Fc2blog\Web\Request;
 use Fc2blog\Web\Session;
 use PHPUnit\Framework\TestCase;
@@ -13,17 +14,32 @@ class IndexTest extends TestCase
 {
   use ClientTrait;
 
-  public function testDefaultSelectedBlog(): void
+  public function testIndex(): void
   {
+    DBHelper::clearDbAndInsertFixture();
     Session::destroy(new Request());
     $this->resetSession();
     $this->resetCookie();
     $this->mergeAdminSession();
 
-    $c = $this->reqGet("/admin/common/index");
-    $this->assertInstanceOf(CommonController::class, $c);
+    $c = $this->reqGet("/admin/blogs/index");
+    $this->assertInstanceOf(BlogsController::class, $c);
     $this->assertEquals("index", $c->getResolvedMethod());
   }
 
-  // TODO ブログを選択
+  public function testChangeBlog(): void
+  {
+    DBHelper::clearDbAndInsertFixture();
+    Session::destroy(new Request());
+    $this->resetSession();
+    $this->resetCookie();
+    $this->mergeAdminSession();
+
+    $this->assertEquals('testblog2', $this->clientTraitSession['blog_id']);
+
+    $r = $this->reqGetBeRedirect("/admin/blogs/choice", ['blog_id' => 'testblog1']);
+    $this->assertEquals('/admin/', $r->redirectUrl);
+
+    $this->assertEquals('testblog1', $this->clientTraitSession['blog_id']);
+  }
 }
