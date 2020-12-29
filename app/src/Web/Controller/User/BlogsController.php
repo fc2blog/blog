c998e8fbe3428ef256385edbb15bb723d593caac
uzulla <uzulla@himitsukichi.com>
2020-12-29 17:42:08 +0900

    remove unused (blog/)index ref:#166

--- app/src/Web/Controller/User/BlogsController.php
+++ app/src/Web/Controller/User/BlogsController.php
@@ -2,7 +2,7 @@
 
 namespace Fc2blog\Web\Controller\User;
 
-use Fc2blog\Model\Model;
+use Fc2blog\Model\BlogsModel;
 use Fc2blog\Web\Request;
 
 class BlogsController extends UserController
@@ -15,11 +15,10 @@ class BlogsController extends UserController
    */
   public function index(Request $request)
   {
-    $blog = Model::load('Blogs')->findByRandom();
+    $blog = (new BlogsModel())->findByRandom();
     if (empty($blog)) {
       return $this->error404();
     }
     $this->redirect($request, $request->baseDirectory . $blog['id'] . '/');
-    return "";
   }
 }
