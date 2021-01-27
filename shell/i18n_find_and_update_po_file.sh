ac294565b3e1f457b1f0aa0beffdb293b2da5a7a
uzulla <uzulla@himitsukichi.com>
2021-01-27 09:25:35 +0900

    Refactoring: move shell script for i18n.
    Move out from app/ dir, these scripts are needed developers only.

--- shell/i18n_find_and_update_po_file.sh
+++ shell/i18n_find_and_update_po_file.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+# Correct translate targets
 echo " ===== Start create po file ===== "
 SELF_DIR=`dirname $0`
 xgettext --keyword=__ --language php --from-code=UTF-8 --output messages.pot `find ${SELF_DIR}/../ -name \*.php -o -name \*.html -type f | grep -v /temp/`
 cat messages.pot | sed "s/plain; charset=CHARSET/plain; charset=UTF-8/" > messages.po
 
-for DIR_LIST in `ls -l ${SELF_DIR}/../locale/ | awk '$1 ~/d/ {print $9}'`
+for DIR_LIST in `ls -l ${SELF_DIR}/../app/locale/ | awk '$1 ~/d/ {print $9}'`
 do
-  if [ ! -e ${SELF_DIR}/../locale/${DIR_LIST}/LC_MESSAGES/messages.po ]
+  if [ ! -e ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po ]
   then
     echo "...Create ${DIR_LIST}/LC_MESSAGES/messages.po"
-    cp messages.po ${SELF_DIR}/../locale/${DIR_LIST}/LC_MESSAGES/messages.po
+    cp messages.po ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po
   else
     echo "...Merge ${DIR_LIST}/LC_MESSAGES/messages.po"
-    msgmerge ${SELF_DIR}/../locale/${DIR_LIST}/LC_MESSAGES/messages.po messages.po -o ${SELF_DIR}/../locale/${DIR_LIST}/LC_MESSAGES/messages.po
+    msgmerge ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po messages.po -o ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po
   fi
 done
 rm -f messages.pot
