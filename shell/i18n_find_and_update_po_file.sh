33fb91b58305d9291e1b883471d4b2133c4b91d7
uzulla <uzulla@himitsukichi.com>
2021-01-27 10:49:15 +0900

    Update xgettext script. support twig.

--- shell/i18n_find_and_update_po_file.sh
+++ shell/i18n_find_and_update_po_file.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Correct translate targets
+# Auto correct translate targets and generate po file, BUT DO NOT TRUST.
 echo " ===== Start create po file ===== "
 SELF_DIR=`dirname $0`
-xgettext --keyword=__ --language php --from-code=UTF-8 --output messages.pot `find ${SELF_DIR}/../ -name \*.php -o -name \*.html -type f | grep -v /temp/`
+# find from php and twig code. find in twig is not perfect.
+xgettext --keyword=_ --keyword=__ --language php --from-code=UTF-8 --output messages.pot `find ${SELF_DIR}/../app/twig_templates ${SELF_DIR}/../app/src  ${SELF_DIR}/../app/config -name \*.php -o -name \*.twig -type f `
 cat messages.pot | sed "s/plain; charset=CHARSET/plain; charset=UTF-8/" > messages.po
 
 for DIR_LIST in `ls -l ${SELF_DIR}/../app/locale/ | awk '$1 ~/d/ {print $9}'`
@@ -13,7 +14,7 @@ do
     cp messages.po ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po
   else
     echo "...Merge ${DIR_LIST}/LC_MESSAGES/messages.po"
-    msgmerge ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po messages.po -o ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po
+    msgmerge --previous --compendium ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po /dev/null messages.po -o ${SELF_DIR}/../app/locale/${DIR_LIST}/LC_MESSAGES/messages.po
   fi
 done
 rm -f messages.pot
