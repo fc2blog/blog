2b62cb4db9cc775b003838008eed64d56492078b
uzulla <uzulla@himitsukichi.com>
2021-02-06 22:23:39 +0900

    Refactoring router.

--- app/src/Web/Router/Router.php
+++ app/src/Web/Router/Router.php
@@ -6,7 +6,6 @@
 
 namespace Fc2blog\Web\Router;
 
-use Fc2blog\Config;
 use Fc2blog\Util\StringCaseConverter;
 use Fc2blog\Web\Controller\Admin\AdminController;
 use Fc2blog\Web\Controller\User\BlogsController;
@@ -24,7 +23,7 @@ class Router
   public function __construct(Request $request)
   {
     $this->request = $request;
-    // TODO if文ベースのルーターから、なんらかのルーターに切り替えたい（Config全廃が前提）
+    // TODO if文ベースのルーターから、なんらかのルーターに切り替えたい
 
     // favicon.ico アクセス時に404をレスポンスし、ブラウザにリトライさせない。
     // しない場合、404扱いからのブログページへリダイレクトが発生し、無駄な資源を消費する。
@@ -35,16 +34,17 @@ class Router
       return;
     }
 
+    // ユーザー用のパラメータを設定する
+    $path = $request->getPath();
+    $paths = $request->getPaths();
+    $args_controller = "mode";
+    $args_action = "process";
+
     if (preg_match('|\A/admin/|u', $request->uri)) { // Admin routing
       $request->urlRewrite = true;
       $request->baseDirectory = '/admin/';
       $this->className = \Fc2blog\Web\Controller\Admin\CommonController::class; // default controller.
       $this->methodName = 'index'; // default method.
-
-      // 管理用のパラメータを設定する
-      $paths = $request->getPaths();
-      $args_controller = Config::get('ARGS_CONTROLLER');
-      $args_action = Config::get('ARGS_ACTION');
 
       if ($request->isArgs($args_controller)) {
         $this->className = "Fc2blog\\Web\\Controller\\Admin\\" . StringCaseConverter::pascalCase($request->get($args_controller)) . "Controller";
@@ -61,30 +61,32 @@ class Router
     } else { // User Routing
       $request->urlRewrite = false;
       $request->baseDirectory = '/';
-      $this->className = BlogsController::class; // default controller.
 
-      // ユーザー用のパラメータを設定する
-      $path = $request->getPath();
-      $paths = $request->getPaths();
-      $args_controller = "mode";
-      $args_action = "process";
+      $default_blog_id = "testblog2";// TODO なんらか設定から引くように
+      $default_blog_id = null;
 
-      if ($request->get($args_controller) === "common") {
-        $this->className = CommonController::class;
-      }
+      // 対象となるblogを決定する
+      if ($path === "/" && !is_null($default_blog_id)) {
+        // `/`であり(blog_idがURLに無く)、デフォルトblog_idが存在する場合、デフォルトblog_idのトップを表示する
+        $blog_id = $default_blog_id;
+        $request->set('blog_id', $blog_id);
+        if (isset($paths[0])) {
+          $sub_path = $paths[0];
+        }
 
-      // blog_idの設定
-      if (isset($paths[0]) && preg_match('|\A[0-9a-zA-Z]+\z|u', $paths[0])) {
-        $this->className = EntriesController::class; // TODO delete
-        $request->set('blog_id', $paths[0]);
-        $blog_id = $paths[0];
-      }
-
-      // `/`の場合
-      if($path === "/"){
+      } else if ($path === "/" && is_null($default_blog_id)) {
+        // `/`であり(blog_idがURLに無く)、デフォルトblog_idが存在しない場合
         $this->className = BlogsController::class;
         $this->methodName = 'index';
+
+      } else if (!is_null($request->getBlogId())) {
+        // blog_idがURLから特定できる場合
+        $blog_id = $request->getBlogId();
+        if (isset($paths[1])) {
+          $sub_path = $paths[1];
+        }
       }
+      // TODO: blog_idがURLから特定できない場合でsub_pathを規定しないといけない？
 
       if (isset($blog_id) && $request->isArgs('xml')) { // `/?xml`
         // RSS feed
@@ -92,23 +94,23 @@ class Router
         $this->methodName = 'feed';
 
       } else if (isset($blog_id) && !$request->isArgs($args_action)) {
-        // トップページ
-        $this->methodName = 'index';
+        $this->className = EntriesController::class;
 
-        if ($request->rawHasGet('no') && $request->isGet()) {
+        if (isset($sub_path) && strpos($sub_path, 'archives.html') === 0) {
+          // アーカイブ
+          $this->methodName = 'archive';
+
+        } else if ($request->rawHasGet('no') && $request->isGet()) {
           // 記事詳細
           $this->methodName = 'view';
           $request->set('id', $request->get('no'));
 
         } else if ($request->isArgs('no') && $request->isArgs('m2')) {
+          // モバイル用 記事詳細（フォーム表示など
           $this->methodName = 'view';
           $request->set('id', $request->get('no'));
 
-        } else if (isset($paths[1]) && strpos($paths[1], 'archives.html') === 0) {
-          // アーカイブ
-          $this->methodName = 'archive';
-
-        } else if (isset($paths[1]) && preg_match('/^blog-entry-([0-9]+)\.html$/u', $paths[1], $matches)) {
+        } else if (isset($sub_path) && preg_match('/^blog-entry-([0-9]+)\.html$/u', $sub_path, $matches)) {
           // 記事詳細
           $this->methodName = 'view';
           $request->set('id', $matches[1]);
@@ -129,6 +131,10 @@ class Router
         } else if ($request->rawHasGet('q')) {
           // 検索
           $this->methodName = 'search';
+
+        } else {
+          // トップページ
+          $this->methodName = 'index';
         }
 
       } else if (preg_match('{/uploads/[0-9a-zA-Z]/[0-9a-zA-Z]/[0-9a-zA-Z]/([0-9a-zA-Z]+)/file/([0-9]+)_([wh]?)([0-9]+)\.(png|gif|jpe?g)$}', $path, $matches)) {
@@ -152,8 +158,33 @@ class Router
         $request->set('height', $matches[5]);
         $request->set('ext', $matches[6]);
 
-      }else if ($this->methodName === "") {
-        // 一つもかからなかった
+      } else if ($request->get($args_controller) === "common" && $request->get($args_action) === "captcha") {
+        // captcha画像生成
+        $this->className = CommonController::class;
+        $this->methodName = 'captcha';
+
+      } else if ($request->get($args_controller) === "common" && $request->get($args_action) === "device_change") {
+        // 機種切り替え、つかわれているケースがあるか不明
+        $this->className = CommonController::class;
+        $this->methodName = 'device_change';
+
+      } else if ($request->get($args_controller) === "common" && $request->get($args_action) === "lang") {
+        // 言語切り替え
+        $this->className = CommonController::class;
+        $this->methodName = 'lang';
+
+      } else if ($request->get($args_controller) === "blogs" && $request->get($args_action) === "index") {
+        // 言語切り替え
+        $this->className = BlogsController::class;
+        $this->methodName = 'index';
+
+      } else if ($this->className === "") {
+        // Class(mode)がargsで指定がなく、ここまでに確定しなければEntriesController
+        $this->className = EntriesController::class;
+      }
+
+      // ここまでmethodが不定の場合、processから決定
+      if ($this->methodName === "") {
         $this->methodName = $request->get($args_action);
       }
     }
