394da36018c2410259515432d3798efc30037d45
uzulla <uzulla@himitsukichi.com>
2021-03-16 16:22:07 +0900

    WIP Implement update feature.

--- app/src/Model/SystemUpdateModel.php
+++ app/src/Model/SystemUpdateModel.php
@@ -3,59 +3,23 @@ declare(strict_types=1);
 
 namespace Fc2blog\Model;
 
+use InvalidArgumentException;
 use JsonException;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use RuntimeException;
+use ZipArchive;
 
 class SystemUpdateModel
 {
   /** @var string */
   static public $releases_url = "https://www.github.com/uzulla/fc2blog/releases"; // TODO: change to fc2blog/blog
   /** @var string */
-//  static public $releases_api_url = "https://api.github.com/repos/uzulla/tag-release-test/releases"; // TODO: change to fc2blog/blog
   static public $releases_api_url = "https://api.github.com/repos/uzulla/fc2blog/releases"; // TODO: change to fc2blog/blog
-
-  /**
-   * Get release info list from GitHub.
-   */
-  public static function getReleaseInfo(): ?array
-  {
-    // TODO cache
-    // app/temp/releases.json の最終更新時刻をみて、1h以内ならそれを利用する
-
-    $options = ['http' => ['header' => "User-Agent: fc2blog_installer"]];
-
-    $releases_json = @file_get_contents(
-      static::$releases_api_url,
-      false,
-      stream_context_create($options)
-    );
-
-    // rate limit等
-    $pos = strpos($http_response_header[0], '403');
-    if ($pos !== false) {
-      var_dump($http_response_header);
-      return null;
-    }
-
-    $pos = strpos($http_response_header[0], '200');
-    if ($pos === false) {
-      throw new RuntimeException("api request failed: status code {$http_response_header[0]}");
-    }
-
-    if ($releases_json === false) {
-      throw new RuntimeException("api request failed");
-    }
-
-    try {
-      $releases_data = json_decode($releases_json, true, 512, JSON_THROW_ON_ERROR);
-    } catch (JsonException $e) {
-      throw new RuntimeException("json parse failed:{$e->getMessage()} on {$e->getFile()}:{$e->getLine()}");
-    }
-
-    // app/temp/releases.json にjsonを書き出す
-
-    return $releases_data;
-  }
+  /** @var string */
+  static public $cached_release_info_path = __DIR__ . "/../../temp/github_release_cache.json";
+  /** @var string */
+  static public $version_file_path = __DIR__ . "/../../version";
 
   /**
    * Get download url of `fc2blog_dist.zip` in assets from release array.
@@ -83,6 +47,65 @@ class SystemUpdateModel
   }
 
   /**
+   * Get release info list from GitHub.
+   * @return array|null
+   */
+  public static function getReleaseInfo(): ?array
+  {
+    // app/temp/releases.json の最終更新時刻をみて、1h以内ならそれを利用する
+    if (file_exists(static::$cached_release_info_path) && filemtime(static::$cached_release_info_path) > (time() - (60 * 60))) {
+      $releases_json = file_get_contents(static::$cached_release_info_path);
+      try {
+        return json_decode($releases_json, true, 512, JSON_THROW_ON_ERROR);
+      } catch (JsonException $e) {
+        error_log("invalid json. but on going.");
+      }
+    }
+
+    // まず削除する
+    if (file_exists(static::$cached_release_info_path)) {
+      unlink(static::$cached_release_info_path);
+    }
+
+    // JSONのrelease情報JSONをGitHub APIより取得
+    $options = ['http' => ['header' => "User-Agent: fc2blog_installer"]];
+    $releases_json = @file_get_contents(
+      static::$releases_api_url,
+      false,
+      stream_context_create($options)
+    );
+
+    // rate limit等
+    $pos = strpos($http_response_header[0], '403');
+    if ($pos !== false) {
+      return null;
+    }
+
+    // 成功か確認
+    $pos = strpos($http_response_header[0], '200');
+    if ($pos === false) {
+      throw new RuntimeException("api request failed: status code {$http_response_header[0]}");
+    }
+    if ($releases_json === false) {
+      throw new RuntimeException("api request failed");
+    }
+
+    // デコード試行
+    try {
+      $releases_data = json_decode($releases_json, true, 512, JSON_THROW_ON_ERROR);
+    } catch (JsonException $e) {
+      // JSONでデコード出来ないデータが来ている
+      error_log("json parse failed:{$e->getMessage()} on {$e->getFile()}:{$e->getLine()}");
+      return null;
+    }
+
+    // app/temp/releases.json にjsonを書き出す
+    file_put_contents(static::$cached_release_info_path, $releases_json);
+
+    return $releases_data;
+  }
+
+  /**
    * Get latest release that has vX.X.X tag from release list.
    * @param $release_list
    * @return array|null
@@ -97,6 +120,151 @@ class SystemUpdateModel
       }
     }
     return null;
+  }
+
+  /**
+   * Get this fc2blog version from version file
+   * @param bool $allow_raw
+   * @return string|null
+   */
+  public static function getVersion(bool $allow_raw = false): ?string
+  {
+    if (!file_exists(static::$version_file_path)) {
+      // 開発中など、不明
+      return null;
+    }
+
+    $version = file_get_contents(static::$version_file_path);
+
+    if ($allow_raw) {
+      return $version;
+    }
+
+    if (!preg_match("/\Av[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\z/u", $version)) {
+      // invalid version format
+      return null;
+    }
+
+    return $version;
+  }
+
+  /**
+   * find release array from release list by version string.
+   * @param array $release_list
+   * @param string $version
+   * @return array|null
+   */
+  public static function findByVersionFromReleaseList(array $release_list, string $version): ?array
+  {
+    foreach ($release_list as $release) {
+      if ($version === $release['tag_name']) {
+        if (isset($release['assets']) && is_array($release['assets']) && count($release['assets']) > 0) {
+          return $release;
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * file download
+   * @param $src
+   * @param $dist
+   * @return bool
+   */
+  private static function downloadFile($src, $dist): bool
+  {
+    // use stream wrapper
+    $download_read_fh = fopen($src, "r");
+    $download_write_fh = fopen($dist, 'w');
+    while (!feof($download_read_fh)) {
+      fwrite($download_write_fh, fread($download_read_fh, 1024 * 1024));
+    }
+    fclose($download_read_fh);
+    fclose($download_write_fh);
+    return true;
+  }
+
+  public static function updateSystemByUrl(string $zip_url)
+  {
+    // テンポラリのURLにダウンロード
+    $file = tmpfile();
+    $zip_path = stream_get_meta_data($file)['uri'];
+
+    if (!static::downloadFile($zip_url, $zip_path)) {
+      throw new RuntimeException("File download error");
+    }
+
+    try {
+      static::updateSystemByLocalZip($zip_path);
+    } finally {
+      @unlink($zip_url);
+    }
+  }
+
+  public static function updateSystemByLocalZip(string $dist_zip_path)
+  {
+    $tmp_path = tempnam(sys_get_temp_dir(), "fc2blog_dist_");
+
+    if (!mkdir($tmp_path)) {
+      throw new RuntimeException("mkdir({$tmp_path}) failed");
+    }
+
+    try {
+      // extract zip in tmp dir
+      $zip = new ZipArchive();
+      if (!$zip->open($dist_zip_path)) {
+        exit("failed open zip: {$dist_zip_path}");
+      }
+      $zip->extractTo($tmp_path);
+
+      // check extract file
+      $tmp_dir_app = $tmp_path . "/app";
+      $tmp_dir_public = $tmp_path . "/public";
+      if (!file_exists($tmp_dir_app) || !file_exists($tmp_dir_public)) {
+        throw new InvalidArgumentException("It seems failed that file extract or invalid zip.");
+      }
+
+      // decide app dir.
+      $app_dir = APP_DIR;
+
+      // deploy files
+      $files_in_tmp_dir_app = glob($tmp_dir_app . '/{*,.[!.]*,..?*}', GLOB_BRACE);
+      foreach ($files_in_tmp_dir_app as $files_in_tmp_dir_app_row) {
+        copy_r($files_in_tmp_dir_app_row, $app_dir); // todo error handling
+      }
+      $files_in_tmp_dir_public = glob($tmp_dir_public . '/{*,.[!.]*,..?*}', GLOB_BRACE);
+      foreach ($files_in_tmp_dir_public as $files_in_tmp_dir_public_row) {
+        copy_r($files_in_tmp_dir_public_row, __DIR__); // todo error handling
+      }
+
+    } finally {
+      static::rmdir_r($tmp_path);
+    }
+
+  }
+
+  private static function rmdir_r(string $dirPath): bool
+  {
+    if (!empty($dirPath) && is_dir($dirPath)) {
+      $dirObj = new RecursiveDirectoryIterator($dirPath, RecursiveDirectoryIterator::SKIP_DOTS);
+      $files = new RecursiveIteratorIterator($dirObj, RecursiveIteratorIterator::CHILD_FIRST);
+
+      // remove include files,dirs,symlinks.
+      foreach ($files as $path) {
+        if ($path->isFile()) {
+          unlink($path->getPathname());
+        } elseif ($path->isLink()) {
+          unlink($path->getPathname());
+        } elseif ($path->isDir()) {
+          rmdir($path->getPathname());
+        }
+      }
+      // remove target.
+      rmdir($dirPath);
+      return true;
+    }
+    return false;
   }
 
 }
--- app/src/Web/Controller/Admin/SystemUpdateController.php
+++ app/src/Web/Controller/Admin/SystemUpdateController.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Fc2blog\Web\Controller\Admin;
 
 use Fc2blog\Model\SystemUpdateModel;
+use Fc2blog\Web\Html;
 use Fc2blog\Web\Request;
+use LogicException;
 
 class SystemUpdateController extends AdminController
 {
@@ -12,13 +14,42 @@ class SystemUpdateController extends AdminController
   {
     $release_list = SystemUpdateModel::getReleaseInfo();
     $this->set('release_list', $release_list);
-
-    // TODO get this system version
-
+    $this->set('repo_site_url', SystemUpdateModel::$releases_url);
+    $this->set('now_version', SystemUpdateModel::getVersion());
     return 'admin/system_update/index.twig';
   }
 
-  // TODO implement update action
+  public function update(Request $request): string
+  {
+    // check sig
+    if(!$request->isValidSig()){
+      $this->setWarnMessage("request failed. please retry."); // TODO i18n
+      $this->redirect($request, Html::url($request, ['controller'=>'ServerUpdate', 'action'=>'index']));
+    }
+
+    // check request
+    $request_version = $request->get('version');
+    if(is_null($request_version)){
+      $this->setWarnMessage("request failed. missing version."); // TODO i18n
+      $this->redirect($request, Html::url($request, ['controller'=>'ServerUpdate', 'action'=>'index']));
+    }
+
+    $release_list = SystemUpdateModel::getReleaseInfo();
+    // get request version
+    $release = SystemUpdateModel::findByVersionFromReleaseList($release_list, $request_version);
+
+    $zip_url = SystemUpdateModel::getZipDownloadUrl($release);
+
+    SystemUpdateModel::updateSystemByUrl($zip_url);
+
+    // add flash
+    $this->setInfoMessage("updated");
+
+    // redirect to index
+    $this->redirect($request, Html::url($request, ['controller'=>'ServerUpdate', 'action'=>'index']));
+
+    throw new LogicException("must be redirect");
+  }
 
 }
 
