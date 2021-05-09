326826e26af9f0355d1c2cef5e44f76794ac3f28
uzulla <uzulla@himitsukichi.com>
2021-05-09 15:32:29 +0900

    code format #304

--- public/index.php
+++ public/index.php
@@ -3,12 +3,12 @@
 // Please set actual `app` directory path to $app_dir_path.
 $app_dir_path = '../app';
 
-if (defined("READ_FROM_INSTALLER")){
-  return $app_dir_path;
+if (defined("READ_FROM_INSTALLER")) {
+    return $app_dir_path;
 }
 
 if (!file_exists($app_dir_path . "/src/include/index_include.php")) {
-  die("please set correct `\$app_dir_path` in index.php ");
+    die("please set correct `\$app_dir_path` in index.php ");
 }
 
 require($app_dir_path . "/src/include/index_include.php");
