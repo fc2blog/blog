a709a87f6851f757d35e9eccfa58fc09a7a23474
uzulla <uzulla@himitsukichi.com>
2021-06-13 18:32:44 +0900

    remove auto select feature of mysqli or pdo. #332

--- app/src/include/bootstrap.php
+++ app/src/include/bootstrap.php
@@ -4,11 +4,7 @@ define('REQUEST_MICROTIME', microtime(true)); // 開始タイムスタンプ(ミ
 define('APP_DIR', realpath(__DIR__ . '/../../') . '/'); // APPディレクトリのパス
 
 // DBの接続ライブラリ
-if (class_exists('mysqli')) {
-    define('DB_CONNECT_LIB', 'mysqli');
-} else if (class_exists('PDO')) {
-    define('DB_CONNECT_LIB', 'PDO');
-}
+define('DB_CONNECT_LIB', 'PDO');
 
 // 環境設定読み込み
 \Fc2blog\Config::read(__DIR__ . '/../config/init_config.php');
