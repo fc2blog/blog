5255c6ec0224320422731b1ee8dfbb6f8c5b0ead
uzulla <uzulla@himitsukichi.com>
2021-06-19 21:50:26 +0900

    remove unnecessary DB connect lib settings.

--- app/src/include/bootstrap.php
+++ app/src/include/bootstrap.php
@@ -1,16 +1,13 @@
 <?php /** @noinspection PhpFullyQualifiedNameUsageInspection */
-
+// TODO init_config.phpと統合できるのではないか？
 define('REQUEST_MICROTIME', microtime(true)); // 開始タイムスタンプ(ミリ秒含む)
 define('APP_DIR', realpath(__DIR__ . '/../../') . '/'); // APPディレクトリのパス
-
-// DBの接続ライブラリ
-define('DB_CONNECT_LIB', 'PDO');
 
 // 環境設定読み込み
 \Fc2blog\Config::read(__DIR__ . '/../config/init_config.php');
 
-// タイムゾーン設定
+// タイムゾーン設定 TODO php.ini移譲でよいのではないか？
 date_default_timezone_set(\Fc2blog\Config::get('TIMEZONE'));
 
-// 内部文字コードを設定
-mb_internal_encoding(\Fc2blog\Config::get('INTERNAL_ENCODING', 'UTF-8'));
+// 内部文字コードを設定 TODO 非推奨なので消し込む
+mb_internal_encoding('UTF-8');
