d0904d12cae2af03c7123cd289997a3a5ebda93e
uzulla <uzulla@himitsukichi.com>
2021-10-10 18:31:29 +0900

    add memo.

--- app/src/Model/PDOConnection.php
+++ app/src/Model/PDOConnection.php
@@ -39,6 +39,7 @@ class PDOConnection
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES => false,
+//                PDO::MYSQL_ATTR_MULTI_STATEMENTS => false // TBD
             ]
         );
     }
