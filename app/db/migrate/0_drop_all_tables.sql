e2f4f8e86d387dce22a11cd1711e1d64f8a26bd8
uzulla <uzulla@himitsukichi.com>
2021-10-17 18:49:32 +0900

    chore

--- app/db/migrate/0_drop_all_tables.sql
+++ app/db/migrate/0_drop_all_tables.sql
@@ -1,28 +1,30 @@
-drop procedure if exists dropalltables;
+drop procedure if exists dropAllTables;
+
 delimiter //
-create procedure dropalltables()
+create procedure dropAllTables()
 begin
-    declare tablename varchar(255);
-    declare isdone int default 0;
-    declare tablenamecursor cursor for
+    declare tableName varchar(255);
+    declare isDone int default 0;
+    declare tableNameCursor cursor for
         select TABLE_NAME
         from `information_schema`.tables
-        where table_schema = database() and table_type = 'BASE TABLE';
-    declare continue handler for sqlstate '02000' set isdone = 1;
-    open tablenamecursor;
+        where table_schema = database()
+          and table_type = 'BASE TABLE';
+    declare continue handler for sqlstate '02000' set isDone = 1;
+    open tableNameCursor;
     repeat
-        fetch tablenamecursor into tablename;
-        if not isdone then
-            set @sql = CONCAT('DROP TABLE ', tablename);
+        fetch tableNameCursor into tableName;
+        if not isDone then
+            set @sql = CONCAT('DROP TABLE ', tableName);
             prepare stmt from @sql;
             execute stmt;
         end if;
-    until isdone end repeat;
-    close tablenamecursor;
+    until isDone end repeat;
+    close tableNameCursor;
 end
 //
 delimiter ;
 
-call dropalltables;
+call dropAllTables;
 
-drop procedure if exists dropalltables;
+drop procedure if exists dropAllTables;
